cb3c66421f421ff5b02de8a693b9d95c48c747c8
VitaliyGavrilov <vitaliy140798gavrilov@mail.ru>
2024-04-09 18:22:54 +0500

    +

--- src/app/pages/components/request-editor-translate/request-editor-translate.component.ts
+++ src/app/pages/components/request-editor-translate/request-editor-translate.component.ts
@@ -1,24 +1,12 @@
-import { emailValidator, innValidator } from './../../../validators/pattern-validator';
-import { Component, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, MinLengthValidator, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, Subject, find, map, pipe, takeUntil, tap, retry, debounce, debounceTime, distinctUntilChanged } from 'rxjs';
-import { ContractorService } from './../../../api/services/contractor.service';
-import { City, Client, ClientGroup, Contractor, ContractorRequestFormat, Country, Currency, Customer, DirectionCity, Employee, FileDocument, TaxSystem, RequestFile } from 'src/app/api/custom_models';
-import { CargoService, CompanyService, CustomerService, DirectionService, RequestService, SystemService, TransportService } from 'src/app/api/services';
-import { Editor } from 'src/app/classes/editor';
-import { Location, getLocaleMonthNames } from '@angular/common';
+import { Subject} from 'rxjs';
+import { CargoService, CustomerService, DirectionService, RequestService, SystemService, TransportService } from 'src/app/api/services';
+import { Location } from '@angular/common';
 import { CityService } from '../../services/city.service';
 import { CountryService } from '../../services/country.service';
-import { byField } from 'src/app/constants';
-import { FileListComponent } from '../file-list/file-list.component';
-import { TransportKind, TransportSubKind, TransportType } from 'src/app/api/custom_models/transport';
-import { Incoterms, Request, RequestFormat, RequestServices } from 'src/app/api/custom_models/request';
-import { CargoPackage, CargoType } from 'src/app/api/custom_models/cargo';
-import { DirectionFlight, DirectionPoint,  } from 'src/app/api/custom_models/direction';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
-import { environment } from './../../../../environments/environment';
 
 @Component({
   selector: 'app-request-editor-translate',
@@ -34,29 +22,16 @@ export class RequestEditorTranslateComponent implements OnInit, OnDestroy {
   requestId:number=0;
   translateAuto:string[]=[];
 
-  isTest=false
-
-
   snackBarWithShortDuration: MatSnackBarConfig = { duration: 1000 };
   snackBarWithLongDuration: MatSnackBarConfig = { duration: 3000 };
 
   private _destroy$ = new Subject();
 
-
-
   constructor(
     private route: ActivatedRoute,
     private fb: FormBuilder,
-    private customerService: CustomerService,
-    private transportService: TransportService,
     private requestService: RequestService,
-    private cargoService: CargoService,
-    private directionService: DirectionService,
-    private countryService: CountryService,
-    private cityService: CityService,
-    private systemService: SystemService,
     private snackBar: MatSnackBar,
-    private location: Location,
     private router: Router,
   ) {
     this.requestFormTranslateRu = this.fb.group({
@@ -137,7 +112,6 @@ export class RequestEditorTranslateComponent implements OnInit, OnDestroy {
       next: () => {
         this.snackBar.open(`Перевод изменен`, undefined, this.snackBarWithShortDuration);
         this.router.navigate(['pages/request/bidding', this.requestId]);
-        
       },
       error: (err) => this.snackBar.open(`Ошибка изменения перевода запроса: ` + err.error.error_message, undefined, this.snackBarWithShortDuration)
     });
@@ -145,7 +119,6 @@ export class RequestEditorTranslateComponent implements OnInit, OnDestroy {
 
   save(){
     this.updateRequestTraqnslate();
-
   }
   remove():void {
     window.location.reload();
