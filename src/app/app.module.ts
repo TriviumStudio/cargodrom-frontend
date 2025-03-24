58a12377e2caf830102409129c45267c3e52a913
VitaliyGavrilov <vitaliy140798gavrilov@mail.ru>
2025-03-24 16:37:53 +0500

    +

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { AuthModule } from './auth/auth.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { Injectable, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +12,39 @@ import { registerLocaleData } from '@angular/common';
 import localeRu from '@angular/common/locales/ru';
 import { PageTitleService } from './page-title.service';
 import { TitleStrategy } from '@angular/router';
+import { NativeDateAdapter } from '@angular/material/core';
+@Injectable()
+export class AppDateAdapter extends NativeDateAdapter {
+  override getFirstDayOfWeek(): number {
+    return 1;
+  }
+  override parse(value: any,): Date | null {
+    if((typeof value === 'string') && (value.indexOf('/') > -1)) {
+      const str = value.split('/');
+      const year = Number(str[0]);
+      const month = Number(str[1]) - 1;
+      const date = Number(str[2]);
+      return new Date(year, month, date);
+    }
+    const timestamp = typeof value === 'number' ? value : Date.parse(value);
+    return isNaN(timestamp) ? null : new Date(timestamp);
+  }
+  override format(date: Date, displayFormat: string): string {
+    if(displayFormat === 'input') {
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      return `${year}/${month}/${day}`;
+    } else if(displayFormat === 'inputMonth') {
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      return `${year}/${month}`;
+    } else {
+      return date.toDateString();
+    }
+  }
+
+}
 
 @NgModule({
   declarations: [
@@ -25,6 +58,8 @@ import { TitleStrategy } from '@angular/router';
     BrowserAnimationsModule,
   ],
   providers: [
+    { provide: NativeDateAdapter, useClass: NativeDateAdapter },
+
     {
       provide: HTTP_INTERCEPTORS,
       useClass: TokenInterceptor,
@@ -42,7 +77,7 @@ import { TitleStrategy } from '@angular/router';
     {
       provide: TitleStrategy,
       useClass: PageTitleService
-    }
+    },
   ],
   bootstrap: [AppComponent]
 })
