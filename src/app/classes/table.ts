0ac203bdd09b1ef732aa79108f26247655d26ef8
Виталий Гаврилов <vitaliy140798gavrilov@mail.ru>
2025-08-18 12:20:54 +0500

    +

--- src/app/classes/table.ts
+++ src/app/classes/table.ts
@@ -147,13 +147,14 @@ export abstract class Table<T extends { id: number }, A = never, F = never> impl
 
   protected loadRows(): void {
     const sortCol = this.getSort();
-    let params: any = { start: this.start, count: this.count, ...this.filter };
+    // let params: any = { start: this.start, count: this.count, ...this.filter ,city_id:['3',1,3] };
+    let params: any = { start: this.start, count: this.count, filter: this.filter };
     if (this.isRateDetailsMode) {
       params = { ...params, request_id: this.requestId, method: this.detailsMethod };
     } else if (this.isBiddingMode) {
       params = { ...params, bidding_request_id: this.requestId };
     } else {
-      params = { ...params, sort: JSON.stringify(sortCol) };
+      params = { ...params, sort: JSON.stringify(sortCol)};
     }
     console.log(params);
 
@@ -170,7 +171,8 @@ export abstract class Table<T extends { id: number }, A = never, F = never> impl
     //   ? { request_id:this.requestId, method: this.detailsMethod, start: this.start, count: this.count, ...this.filter }
     //   : { start: this.start, count: this.count, sort: JSON.stringify(sortCol) as unknown as SortColumn<T>[], ...this.filter  };
 
-    this.load(params)
+
+    this.load(params as any)
     // this.tableService.getRows(params)
       .subscribe(rows => {
         console.log('rows', rows);
